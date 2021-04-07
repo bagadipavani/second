b29be2ea553f4be2036f143852b23433668acf25
bagadipavani <bagadipavani17@gmail.com>
2021-04-07 12:39:38 +0530

    third commit

--- three.c
+++ three.c
@@ -2,11 +2,13 @@
 #include<conio.h>
 void main()
 {
-	int a=3,b=2,c,d,e;
+	int a=3,b=2,c,d,e,f;
 	c=a*b;
 	d=a/b;
 	e=a%b;
+	f=a-b;
 	printf("%d",c);
 	printf("%d",d);
 	printf("%d",e);
+	printf("%d",f);
 }
